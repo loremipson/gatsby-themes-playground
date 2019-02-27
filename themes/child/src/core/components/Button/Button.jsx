cb4255a8ef06d7ce308788a1dcc1da8b3610d21a
Tyler Ipson <tyler.ipson@clearlink.com>
2019-02-27 13:56:42 -0700

    Example with a directory as a component, shadowing one file

---
+++ themes/child/src/core/components/Button/Button.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Button = ({ children }) => <button style={{ fontSize: '20px' }}>{children}</button>
+
+export default Button
--- themes/core/src/pages/index.js
+++ themes/core/src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Hello from '../components/HelloWorld'
+import Button from '../components/Button'
 
 const index = () => {
   return (
     <Layout>
       <Hello />
       <p>This page is coming from core.</p>
+      <Button>I am a button</Button>
     </Layout>
   )
 }
