b84785117490bd4cf45510dbd77f51a5635301a1
Tyler Ipson <tyler.ipson@clearlink.com>
2019-02-26 19:21:54 -0700

    Simple layout and helloworld components

---
+++ themes/core/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Hello from '../components/HelloWorld'
+
+const index = () => {
+  return (
+    <Layout>
+      <Hello />
+      <p>This page is coming from core.</p>
+    </Layout>
+  )
+}
+
+export default index
