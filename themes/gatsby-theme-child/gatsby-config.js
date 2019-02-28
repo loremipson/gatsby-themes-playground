dcbb3a112b1faffbcba403918d1fdc1ce739da9d
Tyler Ipson <tyler.ipson@clearlink.com>
2019-02-28 14:29:56 -0700

    Add page-creator resolve for child theme

--- themes/gatsby-theme-child/gatsby-config.js
+++ themes/gatsby-theme-child/gatsby-config.js
@@ -1,3 +1,11 @@
 module.exports = {
   __experimentalThemes: ['gatsby-theme-core'],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        path: `${__dirname}/src/pages`,
+      },
+    },
+  ],
 }
